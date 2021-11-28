9840e0c8fba52f1229b2e14c2428ff4e2888f4b5
MohamedLemin <MohamedLemin@gmail.com>
2021-11-28 16:03:02 +0100

    my First Project

---
+++ Header.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg bg-dark text-white">
+              <a class="navbar-brand  text-white" href="#">Larave_End_Ajax</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item active">
+                    <a class="nav-link  text-white" href="#">Etudiant <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Matiere</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link  text-white" href="#">Note</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link  text-white disabled">Classe</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
